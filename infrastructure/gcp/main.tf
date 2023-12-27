eaabd63c9a3b71fd307bc5c946fea337259c4d87
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2023-12-27 18:28:28 +0200

    feat: add gcs topic & notifications & subscriptions

--- infrastructure/gcp/main.tf
+++ infrastructure/gcp/main.tf
@@ -6,7 +6,6 @@ provider "google" {
 resource "google_project" "project" {
   name       = var.project_name
   project_id = var.project_id
-
   billing_account = var.billing_account_id
 }
 
@@ -29,7 +28,6 @@ resource "google_service_account" "storage_account" {
 resource "google_project_iam_binding" "storage_admin" {
   project = google_project.project.project_id
   role    = "roles/storage.admin"
-
   members = [
     "serviceAccount:${google_service_account.storage_account.email}"
   ]
@@ -38,8 +36,55 @@ resource "google_project_iam_binding" "storage_admin" {
 resource "google_project_iam_binding" "pubsub_admin" {
   project = google_project.project.project_id
   role    = "roles/pubsub.admin"
-
   members = [
     "serviceAccount:${google_service_account.storage_account.email}"
   ]
+}
+
+resource "google_storage_bucket" "public_bucket" {
+  name          = var.public_bucket_name
+  project       = google_project.project.project_id
+  location      = var.gcs_location
+  force_destroy = true
+}
+
+resource "google_pubsub_topic" "gcs_notifications" {
+  name    = var.public_pubsub_storage_topic_name
+  project = google_project.project.project_id
+}
+
+resource "google_storage_notification" "gcs_notification" {
+  bucket         = google_storage_bucket.public_bucket.name
+  topic          = google_pubsub_topic.gcs_notifications.id
+  event_types    = ["OBJECT_FINALIZE", "OBJECT_METADATA_UPDATE", "OBJECT_DELETE", "OBJECT_ARCHIVE"]
+  payload_format = "JSON_API_V1"
+  depends_on     = [google_pubsub_topic.gcs_notifications]
+}
+
+resource "google_pubsub_topic_iam_binding" "pubsub_storage_publisher" {
+  project = google_project.project.project_id
+  topic   = google_pubsub_topic.gcs_notifications.id
+  role    = "roles/pubsub.publisher"
+
+  members = [
+    "serviceAccount:service-${google_project.project.number}@gs-project-accounts.iam.gserviceaccount.com"
+  ]
+}
+
+
+resource "google_pubsub_topic_iam_binding" "pubsub_publisher" {
+  project = google_project.project.project_id
+  topic   = google_pubsub_topic.gcs_notifications.id
+  role    = "roles/pubsub.publisher"
+  members = [
+    "serviceAccount:${google_service_account.storage_account.email}"
+  ]
+  depends_on = [google_pubsub_topic.gcs_notifications]
+}
+
+resource "google_pubsub_subscription" "gcs_subscription" {
+  name    = var.public_pubsub_storage_subscription_name
+  topic   = google_pubsub_topic.gcs_notifications.id
+  project = google_project.project.project_id
+  ack_deadline_seconds = 20
 }
--- infrastructure/gcp/variables.tf
+++ infrastructure/gcp/variables.tf
@@ -17,3 +17,23 @@ variable "billing_account_id" {
   description = "Billing account id"
   type        = string
 }
+
+variable "public_bucket_name" {
+  description = "Public bucket name Google Cloud Storage"
+  type        = string
+}
+
+variable "public_pubsub_storage_topic_name" {
+  description = "Google Cloud Pub/Sub storae events topic name"
+  type        = string
+}
+
+variable "public_pubsub_storage_subscription_name" {
+  description = "Google Cloud Pub/Sub storage events subscription name"
+  type        = string
+}
+
+variable "gcs_location" {
+  description = "Google Cloud Storage Location"
+  default     = "EU"
+}
