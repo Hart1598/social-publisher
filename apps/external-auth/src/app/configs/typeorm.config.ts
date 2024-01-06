8243671020b55001b7a95cbb5d1f8a73139b8919
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-01-06 20:56:53 +0200

    feat: add typeorm config

---
+++ apps/external-auth/src/app/configs/typeorm.config.ts
@@ -0,0 +1,45 @@
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+import { envPath } from './env.config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+dotenvConfig({ path: envPath });
+
+const entities = [];
+
+
+const baseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  entities,
+  migrations: ['migrations/*{.ts,.js}'],
+  synchronize: false,
+  migrationsRun: true,
+  namingStrategy: new SnakeNamingStrategy(),
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+};
+
+const developmentConfig: TypeOrmModuleOptions = {
+  port: parseInt(`${process.env.DATABASE_PORT}`),
+  ...baseConfig,
+};
+
+const prodConfig: TypeOrmModuleOptions = {
+  extra: {
+    socketPath: process.env.DATABASE_HOST,
+  },
+  ...baseConfig,
+};
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config =
+  isDev ? developmentConfig : prodConfig;
+
+export default config;
+
+export const connectionSource = new DataSource(config as DataSourceOptions);
